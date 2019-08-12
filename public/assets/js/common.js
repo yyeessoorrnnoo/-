da524c60440ff8af092ebe243049341569601e46
yyeessoorrnnoo <1570852184@qq.com>
2019-08-12 09:51:14 +0800

    退出功能

---
+++ public/assets/js/common.js
@@ -0,0 +1,18 @@
+
+  $('#logout').on('click',function(){
+    var isComfirm =confirm('确定要退出吗')
+    if(isComfirm){
+      $.ajax({
+        type:'post',
+        url:'/logout',
+        success:function(){
+          location.href='login.html'
+        },
+        error:function(){
+          alert('退出失败')
+        }
+      })
+    }
+
+  })
+ 
